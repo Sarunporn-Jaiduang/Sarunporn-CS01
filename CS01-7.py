bf0f5bddad990dc516c67870d8725b7e13d88cdd
Sarunporn-Jaiduang <iamchompuzaza@gmail.com>
2021-07-16 09:55:20 +0700

    Create CS01-7.py

---
+++ CS01-7.py
@@ -0,0 +1,21 @@
+A = float(input('A = '))
+if (A <= 10 ):
+    print('ไม่ผ่าน')
+elif (A <= 20 ):
+    print('ปรับปรุง')
+else :
+    print('ดีมาก')
+A = float(input('A = '))
+if (A <= 10 ):
+    print('ไม่ผ่าน')
+elif (A <= 20 ):
+    print('ปรับปรุง')
+else :
+    print('ดีมาก')
+A = float(input('A = '))
+if (A <= 10 ):
+    print('ไม่ผ่าน')
+elif (A <= 20 ):
+    print('ปรับปรุง')
+else :
+    print('ดีมาก')
