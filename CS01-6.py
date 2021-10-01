8af181f90ae2780bc0a7693055bbce83e60565bb
Sarunporn-Jaiduang <iamchompuzaza@gmail.com>
2021-10-01 08:00:11 +0700

    Update CS01-6.py

--- CS01-6.py
+++ CS01-6.py
@@ -1,3 +1,9 @@
-A = float(input())
-if(A > 5):
-    print("yes")
+A = float(input('A = '))
+B = float(input('B = '))
+C = float(input('C = '))
+if (A <= 10 ):
+    print('สอบไม่ผ่าน')
+elif (A <= 20 ):
+    print('สอบผ่าน')
+else :
+    print('ดีมาก')
