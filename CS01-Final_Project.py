e4b0fdd8c935e972d31cdc724d7c56c2230af72c
Sarunporn-Jaiduang <iamchompuzaza@gmail.com>
2021-10-11 20:37:36 +0700

    Update CS01-Final_Project.py

--- CS01-Final_Project.py
+++ CS01-Final_Project.py
@@ -1,104 +1,62 @@
 from tkinter import *
-
+from tkinter import font
 root =Tk()
-root.title('Test')
+root.title('calculator')
 
-e = Entry(root, width=35, borderwidth=5)
-e.grid(row=0, column=0, columnspan=3, padx=10,pady=10)
+content = ""
+txt_input = StringVar(value="0")
 
-def button_click(number):
-    current = e.get()
-    e.delete(0, END)
-    e.insert(0,str(current) + str(number))
+#7+10/5*9
+def btn(number):
+    global content
+    content = content+str(number)
+    txt_input.set(content)
 
-def button_clear():
-    e.delete(0, END)
+def equal():
+    global content
+    calculate=float(eval(content))
+    txt_input.set(content)
+    content = ""
 
-def button_add():
-    first_number = e.get()
-    global f_num 
-    global math
-    math = 'addition'
-    f_num = int(first_number)
-    e.delete(0, END)
+def clear():
+    global content
+    content = ""
+    txt_input.set("")
+    display.insert(0,"0")
 
-def button_equal():
-    second_number = e.get()
-    e.delete(0, END)
+#แสดงผล 5*4
+display = Entry(font=('arial',30,'bold'),fg="white",bg="grey",textvariable=txt_input,justify="right")
+display.grid(columnspan=4)
 
-    if math == 'addition':
-        e.insert(0, f_num + int(second_number))
+#รับค่าผ่านปุ่ม
 
-    if math == 'subtraction':
-        e.insert(0, f_num - int(second_number))
+#row1
+btn7=Button(fg="black",font=('arial',30,'bold'),text="7",command=lambda:btn(7),padx=30,pady=15).grid(row=1,column=0)
+btn8=Button(fg="black",font=('arial',30,'bold'),text="8",command=lambda:btn(8),padx=30,pady=15).grid(row=1,column=1)
+btn9=Button(fg="black",font=('arial',30,'bold'),text="9",command=lambda:btn(9),padx=30,pady=15).grid(row=1,column=2)
+btnC=Button(fg="black",bg="orange",font=('arial',30,'bold'),command=lambda:btn("+"),padx=32,pady=15).grid(row=1,column=3)
 
-    if math == 'multiplication':
-        e.insert(0, f_num * int(second_number))
+#row2
+btn4=Button(fg="black",font=('arial',30,'bold'),text="4",command=lambda:btn(4),padx=30,pady=15).grid(row=2,column=0)
+btn5=Button(fg="black",font=('arial',30,'bold'),text="5",command=lambda:btn(5),padx=30,pady=15).grid(row=2,column=1)
+btn6=Button(fg="black",font=('arial',30,'bold'),text="6",command=lambda:btn(6),padx=30,pady=15).grid(row=2,column=2)
+btnplus=Button(fg="black",bg="orange",font=('arial',30,'bold'),command=lambda:btn("+"),padx=32,pady=15).grid(row=2,column=3)
 
-    if math == 'division':
-        e.insert(0, f_num / int(second_number))
+#row3
+btn1=Button(fg="black",font=('arial',30,'bold'),command=lambda:btn(1),text="1",padx=30,pady=15).grid(row=3,column=0)
+btn2=Button(fg="black",font=('arial',30,'bold'),command=lambda:btn(2),text="2",padx=30,pady=15).grid(row=3,column=1)
+btn3=Button(fg="black",font=('arial',30,'bold'),command=lambda:btn(3),text="3",padx=30,pady=15).grid(row=3,column=2)
+btnminus=Button(fg="black",bg="orange",font=('arial',30,'bold'),command=lambda:btn("+"),padx=32,pady=15).grid(row=3,column=3)
 
-def button_subtract():
-    first_number = e.get()
-    global f_num 
-    global math
-    math = 'subtraction'
-    f_num = int(first_number)
-    e.delete(0, END)
+#row4
+btndot=Button(fg="black",bg="orange",font=('arial',30,'bold'),text=".",command=lambda:btn("."),padx=35,pady=15).grid(row=4,column=0)
+btn0=Button(fg="black",font=('arial',30,'bold'),text="0",command=lambda:btn(0),padx=30,pady=15).grid(row=4,column=1)
+btndivision=Button(fg="black",bg="orange",font=('arial',30,'bold'),text="/",command=lambda:btn("/"),padx=36,pady=15).grid(row=4,column=2)
+btnmultiply=Button(fg="black",bg="orange",font=('arial',30,'bold'),text="x",command=lambda:btn("x"),padx=33,pady=15).grid(row=4,column=3)
 
-def button_multiply():
-    first_number = e.get()
-    global f_num 
-    global math
-    math = 'multiplication'
-    f_num = int(first_number)
-    e.delete(0, END)
-
-def button_divide():
-    first_number = e.get()
-    global f_num 
-    global math
-    math = 'division'
-    f_num = int(first_number)
-    e.delete(0, END)
-
-button_1 = Button(root, text='1', padx=40, pady=20, command=lambda: button_click(1))
-button_2 = Button(root, text='2', padx=40, pady=20, command=lambda: button_click(2))
-button_3 = Button(root, text='3', padx=40, pady=20, command=lambda: button_click(3))
-button_4 = Button(root, text='4', padx=40, pady=20, command=lambda: button_click(4))
-button_5 = Button(root, text='5', padx=40, pady=20, command=lambda: button_click(5))
-button_6 = Button(root, text='6', padx=40, pady=20, command=lambda: button_click(6))
-button_7 = Button(root, text='7', padx=40, pady=20, command=lambda: button_click(7))
-button_8 = Button(root, text='8', padx=40, pady=20, command=lambda: button_click(8))
-button_9 = Button(root, text='9', padx=40, pady=20, command=lambda: button_click(9))
-button_0 = Button(root, text='0', padx=40, pady=20, command=lambda: button_click(0))
-button_add = Button(root, text='+', padx=39, pady=20, command=button_add)
-button_equal = Button(root, text='=', padx=87, pady=20, command=button_equal)
-button_clear = Button(root, text='Clear', padx=77, pady=20, command=button_clear)
-
-button_subtract = Button(root, text='-', padx=41, pady=20, command=button_subtract)
-button_multiply = Button(root, text='*', padx=40, pady=20, command=button_multiply)
-button_divide = Button(root, text='/', padx=41, pady=20, command=button_divide)
-
-button_1.grid(row=3, column=0)
-button_2.grid(row=3, column=1)
-button_3.grid(row=3, column=2)
-
-button_4.grid(row=2, column=0)
-button_5.grid(row=2, column=1)
-button_6.grid(row=2, column=2)
-
-button_7.grid(row=1, column=0)
-button_8.grid(row=1, column=1)
-button_9.grid(row=1, column=2)
-
-button_0.grid(row=4, column=0)
-button_clear.grid(row=4, column=1,columnspan=2)
-button_add.grid(row=5, column=0)
-button_equal.grid(row=5, column=1,columnspan=2)
-
-button_subtract.grid(row=6, column=0)
-button_multiply.grid(row=6, column=1)
-button_divide.grid(row=6, column=2)
+#row5
+btnequal=Button(fg="black",bg="cyan",font=('arial',30,'bold'),text="=",command=equal,padx=93,pady=15).grid(row=5,column=0,columnspan=2)
+btnopen=Button(fg="black",bg="orange",font=('arial',30,'bold'),text="(",command=lambda:btn("("),padx=36,pady=15).grid(row=5,column=2)
+btnclose=Button(fg="black",bg="orange",font=('arial',30,'bold'),text=")",command=lambda:btn(")"),padx=36,pady=15).grid(row=5,column=3)
 
 root.mainloop()
